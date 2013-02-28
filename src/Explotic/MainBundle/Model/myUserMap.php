3a240a62444d685b7343b6e9a770e5622840d110
wooddev <adrien.arraiolos@fcba.fr>
2013-02-28 17:26:17 +0100

    Revert ""
    This reverts commit 5fa8cb8a72ce181c809450012efaa96205ba4751.

---
+++ src/Explotic/MainBundle/Model/myUserMap.php
@@ -0,0 +1,17 @@
+<?php
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of myUserMap
+ *
+ * @author arraiolosa
+ */
+class myUserMap {
+    //put your code here
+}
+
+?>
